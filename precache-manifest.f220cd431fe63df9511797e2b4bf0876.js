4cd5b5a6d23c20682f913202538bab839928e9fc
Roman <spleshdamage@gmail.com>
2020-10-23 02:30:15 +0300

    Updates

---
+++ precache-manifest.f220cd431fe63df9511797e2b4bf0876.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f918f2125121f6100c636fdd23788e60",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "4a8c4a74071ffea77f2a",
+    "url": "/react-deploy/static/css/main.af08efbe.chunk.css"
+  },
+  {
+    "revision": "5e6ca683b04838fb8052",
+    "url": "/react-deploy/static/js/2.d9edd779.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-deploy/static/js/2.d9edd779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a8c4a74071ffea77f2a",
+    "url": "/react-deploy/static/js/main.4e1afd40.chunk.js"
+  },
+  {
+    "revision": "86d98ee0cb1baf327b93",
+    "url": "/react-deploy/static/js/runtime-main.a7f1a508.js"
+  }
+]);
